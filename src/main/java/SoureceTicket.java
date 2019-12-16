3734d4ffe65d5fffb189889bda5076434a11569d
cschenruidi <cschenruidi@163.com>
2019-12-16 22:11:10 +0800

    first commit

---
+++ src/main/java/SoureceTicket.java
@@ -0,0 +1,24 @@
+public class SoureceTicket {
+    /*
+    定义实列变量和方法
+    高内聚变量和方法
+     */
+    //票的张数
+    private int number = 30;
+
+    //list list = new ArrayList();
+    //卖票的方法(重锁粒度不好)
+//    public synchronized  void sale(){
+//
+//    }
+    //卖票的方法（lock）
+    /*
+    api中
+    lock 1= 。。。。；
+    1.lock（）；
+    try
+
+     */
+    public void sale(){
+   }
+}
---
+++ src/main/java/Tset.java
@@ -0,0 +1,20 @@
+public class Tset {
+    /*
+    题目：三个人卖三十张票
+    前提：高内聚低耦合
+    1.线程   操作    资源类
+
+        1.1创建一个资源类
+     */
+    /*
+    主线程一切资源的入口
+     */
+    public static void main(String[] args) {
+        /*
+        四个线程 t1，t2，t3，main
+         */
+        Thread t1 = new Thread();
+        Thread t2 = new Thread();
+        Thread t3 = new Thread();
+    }
+}
